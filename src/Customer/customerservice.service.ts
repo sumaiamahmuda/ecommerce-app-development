4d0f42f022d9fae0940ba591ac265284338eb5ac
sumaiamahmuda <124588545+sumaiamahmuda@users.noreply.github.com>
2023-03-09 19:05:05 +0600

    Add files via upload

--- src/Customer/customerservice.service.ts
+++ src/Customer/customerservice.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from "@nestjs/common";
 import { customerForm } from "./customerfrom.dto";
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CustomerEntity } from "./customerentity.entity";
 
 
 @Injectable()
 export class CustomerService {
+    constructor(
+        @InjectRepository(CustomerEntity)
+        private adminRepo: Repository<CustomerEntity>,
+      ) {}
 
 
-
-insertUser(mydto:customerForm):any {
+/*insertUser(mydto:customerForm):any {
     
        return "customer Inserted name: " + mydto.id+" and id is " + mydto.name;
-    }
+    }*/
+
+    insertUser(mydto:customerForm):any {
+        const adminaccount = new CustomerEntity()
+        adminaccount.name = mydto.name;
+        adminaccount.email = mydto.email;
+        adminaccount.password = mydto.password;
+        //adminaccount.address = mydto.address;
+       return this.adminRepo.save(adminaccount);
+          }
+
+          getuser(id):any{
+            return this.adminRepo.findOneBy({id});
+          }
+
+
+          deleteuserbyid(id):any {
+    
+            return this.adminRepo.delete(id);
+        }
 
 updateUser(name,id):any {
         return "customer updated name: " +name+" and id is " +id;
