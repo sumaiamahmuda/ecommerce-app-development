e08ca1e414d0906663220dd80e7065591f3d8588
sumaiamahmuda <124588545+sumaiamahmuda@users.noreply.github.com>
2023-03-09 19:04:23 +0600

    Add files via upload

--- src/Customer/customerfrom.dto.ts
+++ src/Customer/customerfrom.dto.ts
@@ -1,13 +1,16 @@
 import { IsInt, IsNotEmpty, Length } from "class-validator";
 
 export class customerForm {   
-     @IsNotEmpty({message: "Please enter your id"}) 
-     @IsInt()
-     id: number;
+     //@IsNotEmpty({message: "Please enter your id"}) 
+    // @IsInt()
+    // id: number;
  
      @IsNotEmpty()
      @Length(3,8)
      name: string;
+     email:string;
+     password:string;
+    // address:string
  
  
  
