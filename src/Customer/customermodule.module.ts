c08ad19edba9095d26de3e11fc7fca66d5d72be1
sumaiamahmuda <124588545+sumaiamahmuda@users.noreply.github.com>
2023-03-09 19:03:39 +0600

    Add files via upload

--- src/Customer/customermodule.module.ts
+++ src/Customer/customermodule.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { customerController } from './customer.controller';
 import { CustomerService } from './customerservice.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CustomerEntity } from './customerentity.entity';
 
 
 @Module({
-
+  imports: [TypeOrmModule.forFeature([CustomerEntity])],
   controllers: [customerController],
   providers: [CustomerService],
 })
